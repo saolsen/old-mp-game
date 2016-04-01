a64078f2e1c633fef238a829fed29f2a3ec94644
Stephen Olsen <steve.olsen@gmail.com>
2016-04-01 12:59:06 -0500

    ping from client to server

--- src/game_client.c
+++ src/game_client.c
@@ -1,7 +1,48 @@
 #define GAME_CLIENT
 #include "game_platform.h"
 
+// Set this in build system for prod builds to be the real server.
+#ifndef SERVER_HOST
+#define SERVER_HOST "localhost"
+#endif
+
 int main()
 {
-    SDL_Log("Hello World\n");
+    SDL_Log("Hello World, This is the client!\n");
+
+    if (SDL_Init(0) == -1) {
+        SDL_LogError("SDL_Init: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    if (SDLNet_Init() == -1) {
+        SDL_LogError("SDLNet_Init: %s\n", SDLNet_GetError());
+        exit(2);
+    }
+
+    UDPsocket udpsock = SDLNet_UDP_Open(0);
+    if (!udpsock) {
+        SDL_LogError("SDLNet_UDP_Open: %s\n", SDLNet_GetError());
+        exit(2);
+    }
+
+    // Ping the server.
+    // I don't really like that you have to call this to allocate packets. I'd rather do the memory management myself. I had that system with an arena that was working very nicely. That way I don't have to resize.
+    char *data = "Hello I am the client";
+    int len = strlen(data) + 1;
+    UDPpacket *packet = SDLNet_AllocPacket(len);
+    if (!packet) {
+        SDL_LogError("SDLNet_AllocPacket: %s\n", SDLNet_GetError());
+    }
+
+    memcpy(packet->data, data, len);
+    packet->len = len;
+
+    IPaddress address;
+    SDLNet_ResolveHost(&address, SERVER_HOST, 1234);
+    packet->address.host = address.host;
+    packet->address.port = address.port;
+    
+    SDLNet_UDP_Send(udpsock, -1, packet);
+    SDLNet_FreePacket(packet);
 }
--- src/game_server.c
+++ src/game_server.c
@@ -1,7 +1,60 @@
+/* I'm currently using sdl as my standard library but I may want to actually use linux stuff
+   at least for the server.
+*/
+
 #define GAME_SERVER
 #include "game_platform.h"
 
 int main()
 {
+    // @TODO: My own logging macros, I don't like these sdl ones.
     SDL_Log("Hello World, This is the server!\n");
+
+    if (SDL_Init(0) == -1) {
+        SDL_LogError("SDL_Init: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    if (SDLNet_Init() == -1) {
+        SDL_LogError("SDLNet_Init: %s\n", SDLNet_GetError());
+        exit(2);
+    }
+
+    UDPsocket udpsock = SDLNet_UDP_Open(1234);
+    if (!udpsock) {
+        SDL_LogError("SDLNet_UDP_Open: %s\n", SDLNet_GetError());
+        exit(2);
+    }
+
+    // I don't think I want to use SDLNet udp channels. That seems useful for like a broadcast
+    // thing but I think I want more control over how that's going to happen.
+    // I want to do a lot of the stuff enet does, including acks and other ways of doing reliability.
+
+    int running = 1;
+    while (running) {
+        UDPpacket *packet = SDLNet_AllocPacket(1024);
+        int numrecv;
+
+        /* SDL_Log("Checking the network for packets\n"); */
+        // Handle incoming packets.
+        while (SDLNet_UDP_Recv(udpsock, packet) > 0) {
+            SDL_Log("Received Packet: %s\n", packet->data);
+            printf("channel: %i, data: %s, len: %i, maxlen: %i, status: %i: host: %u, port: %u",
+                   packet->channel,
+                   packet->data,
+                   packet->len,
+                   packet->maxlen,
+                   packet->status,
+                   packet->address.host,
+                   packet->address.port);
+        }
+
+        // chill 4 a sec.
+        SDL_Delay(1000);
+    }
+
+    // Cleanup
+    SDLNet_UDP_Close(udpsock);
+    udpsock = NULL;
+    SDLNet_Quit();
 }
